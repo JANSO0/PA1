c218ce7cfa87812a822867427a42aaa8062a9ec6
JANSO0 <jaimealonsoreal@gmail.com>
2024-10-31 16:09:41 +0100

    Emmiter.h

--- desarrollopa/Emmiter.h
+++ desarrollopa/Emmiter.h
@@ -1,13 +1,17 @@
 #pragma once
+#include <vector>
 #include "Solid.h"
+#include "EmmiterConfiguration.h"
+using namespace std;
 
 class Emmiter : public Solid
 {
 private:
-
+	vector<Solid*> refparticulas;
 public:
 
 
 	void Render();
+	void Update();
 };
 
