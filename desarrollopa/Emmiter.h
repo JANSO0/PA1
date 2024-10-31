4203f7f092bda2fded93e8a54a8fa3e28fa7c1be
JANSO0 <jaimealonsoreal@gmail.com>
2024-10-31 16:20:46 +0100

    He añadido un par de propiedades a emmiter.h

--- desarrollopa/Emmiter.h
+++ desarrollopa/Emmiter.h
@@ -1,15 +1,23 @@
 #pragma once
 #include <vector>
+#include <chrono>
 #include "Solid.h"
 #include "EmmiterConfiguration.h"
 using namespace std;
+using namespace std::chrono;
 
 class Emmiter : public Solid
 {
 private:
 	vector<Solid*> refparticulas;
-public:
+	EmmiterConfiguration emisor;
 
+	//TIEMPO//
+	milliseconds initialMilliseconds;
+	long lastUpdateTime;
+
+public:
+	Emmiter(){}
 
 	void Render();
 	void Update();
